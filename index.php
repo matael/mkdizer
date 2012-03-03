25f233594cbacfab5c2b7de59e683ac54672406d
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2012-03-04 00:22:18 +0100

    Add main script

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once "./markdown.php";
+
+$file = './'.$_GET['n'].'.mkd';
+$title = preg_replace('#_#', ' ', $file);
+$fh = fopen($file, 'r');
+
+echo<<<END
+
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>$title</title>
+        <meta charset="utf-8"/>
+	<link rel="stylesheet" media="all" href="main.css" type="text/css"/>
+	</head>
+    <body>
+        <header>
+            <h1>$titre</h1>
+        </header>
+        <article>
+END;
+echo Markdown(fread($fh, filesize($file)));
+
+echo<<<END
+        </article>
+    </body>
+</html>
+END;
+
+?>
